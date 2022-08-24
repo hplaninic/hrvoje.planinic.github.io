7b1213f28f55cd425a411d998019b7f2581fde71
fbaeriswyl <fabien.baeriswyl@unil.ch>
2022-08-24 15:29:43 +0200

    lol

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Mon second poste!",
+        "excerpt":"je fais un deuxième poste aussi car je suis tout à fait disposé à le faire mais en fait c’était le premier lol  ","categories": [],
+        "tags": [],
+        "url": "/mon.second.poste/",
+        "teaser": null
+      },{
+        "title": "Mon premier post!",
+        "excerpt":"Il faut essayer  beaucoup de choses pour qu’un  site prenne forme sur jekyll…   ici c’est un essai pour un test scientifique de haute facture    search:false  ","categories": [],
+        "tags": [],
+        "url": "/mon.premier.post/",
+        "teaser": null
+      }]
